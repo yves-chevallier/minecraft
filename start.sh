1ecdc7971e298626dadc2eb73eea67bb68a03aa3
Yves Chevallier <yves-chevallier@heig-vd.ch>
2020-05-28 22:22:34 +0200

    Fix

--- start.sh
+++ start.sh
@@ -3,11 +3,7 @@
 HEAP_SIZE=4096
 JAR_NAME=/home/minecraft/server/forge-*.jar
 
-# Compute the nursery size.
-NURSERY_MINIMUM=$(($HEAP_SIZE / 2))
-NURSERY_MAXIMUM=$(($HEAP_SIZE * 4 / 5))
-
 # Launch the server.
-CMD="java -Xms${HEAP_SIZE}M -Xmx${HEAP_SIZE}M -Xmns${NURSERY_MINIMUM}M -Xmnx${NURSERY_MAXIMUM}M -Xgc:concurrentScavenge -Xgc:dnssExpectedTimeRatioMaximum=3 -Xgc:scvNoAdaptiveTenure -Xdisableexplicitgc -Xtune:virtualized -jar ${JAR_NAME}"
+CMD="java -Xms${HEAP_SIZE}M -Xmx${HEAP_SIZE}M -jar ${JAR_NAME}"
 echo "launching server with command line: ${CMD}"
 ${CMD}
